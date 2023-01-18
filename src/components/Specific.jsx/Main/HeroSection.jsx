6c43f08b2f0f00fbb66f679661f06cc7d1428c50
abdullah131566 <107644072+abdullah131566@users.noreply.github.com>
2023-01-18 11:34:49 +0500

    Update HeroSection.jsx

--- src/components/Specific.jsx/Main/HeroSection.jsx
+++ src/components/Specific.jsx/Main/HeroSection.jsx
@@ -5,8 +5,10 @@ const HeroSection = () => {
     <section className="section hero-section color-text flex flex-align-center font-weight-light">
       <div className="content-container">
         <h1 className="verse">
-          &lquot;HE MUST INCREASE; <br />
-          AND I MUST DECREASE. &rdquo;
+          <q>
+            HE MUST INCREASE; <br />
+            AND I MUST DECREASE.
+          </q>
         </h1>
         <h5 className="reference">-- JOHN 3:31</h5>
       </div>
