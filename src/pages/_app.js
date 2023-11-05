386019a5cc4c8c9ee58e5647ea62988cbdd9f4fb
Hassaan Bin Sajid <hassaan.bs25@gmail.com>
2023-11-05 21:59:13 +0500

    added Local page
    Local Page Added with Images,
    Routes Added for women-ministry and Global Pages
    Structure Ready

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,10 @@
 import '@/styles/globals.css'
 import '@/styles/general.css'
+import '@/styles/local.css'
 import '@/styles/specific.css'
 import '@/styles/util_classes.css'
+import '@/styles/global-page.css'
+import '@/styles/women-ministry.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
